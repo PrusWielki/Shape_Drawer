d1bcc50a28d979f954f0900573a5cd250ec21975
Patryk Prusak <01151475@pw.edu.pl>
2022-05-03 13:04:58 +0200

    add drawing one line

--- Shape_Drawer/MainWindow.xaml.cs
+++ Shape_Drawer/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,14 +15,34 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Color = System.Windows.Media.Color;
 
 namespace Shape_Drawer
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+    enum Mode
+    {
+        None,
+        Line,
+        Circle,
+
+    }
     public partial class MainWindow : Window
     {
+        System.Windows.Point a;
+        System.Windows.Point b;
+        bool isASet;
+        bool isBSet;
+        SymmetricLine symmetric = new SymmetricLine();
+
+        Mode mode=Mode.None;
+
+
+        System.Drawing.Image imageDrawing;
 
         public IEnumerable<KeyValuePair<String, Color>> NamedColors
         {
@@ -38,7 +61,7 @@ namespace Shape_Drawer
 
         }
 
-        private IEnumerable<KeyValuePair<String, Color>> GetColors()
+        private IEnumerable<KeyValuePair<String, System.Windows.Media.Color>> GetColors()
         {
             return typeof(Colors)
                 .GetProperties()
@@ -47,16 +70,75 @@ namespace Shape_Drawer
                 .Select(prop =>
                     new KeyValuePair<String, Color>(prop.Name, (Color)prop.GetValue(null)));
         }
+        private void ConvertToDrawing(System.Windows.Controls.Image img)
+        {
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            System.Windows.Media.Imaging.BmpBitmapEncoder bbe = new BmpBitmapEncoder();
+            if (null != img.Source)
+                bbe.Frames.Add(BitmapFrame.Create(new Uri("../../../transparent.png", UriKind.RelativeOrAbsolute)));
 
+            else
+                return;
+
+
+            bbe.Save(ms);
+            imageDrawing = System.Drawing.Image.FromStream(ms);
+        }
+        private BitmapImage ToWpfImage(System.Drawing.Image img)
+        {
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            img.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+
+            BitmapImage ix = new BitmapImage();
+            ix.BeginInit();
+            ix.CacheOption = BitmapCacheOption.OnLoad;
+            ix.StreamSource = ms;
+            ix.EndInit();
+            return ix;
+        }
         private void backgroundImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            MessageBox.Show("XDXD");
             System.Windows.Point p = e.GetPosition(backgroundImage);
+            // MessageBox.Show(p.X + ", " + p.Y);
             double pixelWidth = backgroundImage.Source.Width;
             double pixelHeight = backgroundImage.Source.Height;
             double x = pixelWidth * p.X / backgroundImage.ActualWidth;
             double y = pixelHeight * p.Y / backgroundImage.ActualHeight;
-            MessageBox.Show(x + ", " + y);
+            if (mode != Mode.None)
+            {
+                if (!isASet)
+                {
+                    a = new System.Windows.Point(x, y);
+                    isASet = true;
+                }
+                else if (!isBSet)
+                {
+                    b = new System.Windows.Point(x, y);
+                    isBSet = true;
+
+                }
+                if (isASet && isBSet)
+                {
+
+                    if (mode == Mode.Line)
+                    {
+                        ConvertToDrawing(backgroundImage);
+                        imageDrawing = symmetric.Draw(a, b, imageDrawing);
+                        backgroundImage.Source = ToWpfImage(imageDrawing);
+                        mode = Mode.None;
+
+                    }
+
+                }
+
+            }
+            
+            //MessageBox.Show(x + ", " + y);
+        }
+
+        private void DrawALineButton_Click(object sender, RoutedEventArgs e)
+        {
+            mode = Mode.Line;
         }
     }
 }
--- Shape_Drawer/ShapeDrawer.cs
+++ Shape_Drawer/ShapeDrawer.cs
@@ -1,44 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using Point = System.Windows.Point;
 
 namespace Shape_Drawer
 {
     internal abstract class ShapeDrawer
     {
-        abstract public void Draw();
+        abstract public Image Draw(Point a, Point b, Image imgSource);
 
     }
 
     internal class SymmetricLine : ShapeDrawer
     {
-        public override void Draw()
+        public override Image Draw(Point a, Point b, Image imgSource)
         {
-           /* int dx = x2 ‐ x1;
-            int dy = y2 ‐ y1;
-            int d = 2 * dy ‐ dx;
+
+            int width = imgSource.Width;
+            int height = imgSource.Height;
+            Bitmap newBitmap = (Bitmap)imgSource.Clone();
+
+
+            BitmapData srcData = newBitmap.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            //hold the amount of bytes needed to represent the image's pixels
+            int bytes = srcData.Stride * srcData.Height;
+            byte[] buffer = new byte[bytes];
+            byte[] result = new byte[bytes];
+
+            //copy image data to the buffer
+            Marshal.Copy(srcData.Scan0, buffer, 0, bytes);
+            newBitmap.UnlockBits(srcData);
+
+            int current = 0;
+
+            int dx = (int)(a.X - b.X);
+            int dy = (int)(a.Y - b.Y);
+
+            int d = 2 * dx - dy;
             int dE = 2 * dy;
-            int dNE = 2 * (dy ‐ dx);
-            int xf = x1, yf = y1;
-            int xb = x2, yb = y2;
-            putPixel(xf, yf);
-            putPixel(xb, yb);
+            int dNE = 2 * (dy - dx);
+
+            int xf = (int)a.X, yf = (int)a.Y;
+            int xb = (int)b.X, yb = (int)b.Y;
+
+            buffer[yf * srcData.Stride + xf * 4] = (byte)0;
+            buffer[yf * srcData.Stride + xf * 4+1] = (byte)0;
+            buffer[yf * srcData.Stride + xf * 4+2] = (byte)0;
+
+
+            buffer[yb * srcData.Stride + xb * 4] = (byte)0;
+            buffer[yb * srcData.Stride + xb * 4 + 1] = (byte)0;
+            buffer[yb * srcData.Stride + xb * 4 + 2] = (byte)0;
+
+
             while (xf < xb)
             {
-                ++xf; ‐‐xb;
+                ++xf; --xb;
                 if (d < 0)
                     d += dE;
                 else
                 {
                     d += dNE;
                     ++yf;
-                    ‐‐yb;
+                    --yb;
                 }
-                putPixel(xf, yf);
-                putPixel(xb, yb);
-            }*/
+                buffer[yf * srcData.Stride + xf * 4] = (byte)0;
+                buffer[yf * srcData.Stride + xf * 4 + 1] = (byte)0;
+                buffer[yf * srcData.Stride + xf * 4 + 2] = (byte)0;
+
+
+                buffer[yb * srcData.Stride + xb * 4] = (byte)0;
+                buffer[yb * srcData.Stride + xb * 4 + 1] = (byte)0;
+                buffer[yb * srcData.Stride + xb * 4 + 2] = (byte)0;
+            }
+
+
+            //create a new bitmap with changed pixel rgb values
+            Bitmap resImg = new Bitmap(width, height);
+            BitmapData resData = resImg.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+            Marshal.Copy(buffer, 0, resData.Scan0, bytes);
+            resImg.UnlockBits(resData);
+
+            return resImg;
+
+
+
+            
+           // putPixel(xf, yf);
+           // putPixel(xb, yb);
+            
         }
     }
 }
