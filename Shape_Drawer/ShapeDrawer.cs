0b81cce286b655e235abf708ca9c8a6dfca34988
Patryk Prusak <01151475@pw.edu.pl>
2022-05-23 00:33:44 +0200

    remove unused usings

--- Shape_Drawer/ShapeDrawer.cs
+++ Shape_Drawer/ShapeDrawer.cs
@@ -2,11 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 namespace Shape_Drawer
 {
     public struct Point
